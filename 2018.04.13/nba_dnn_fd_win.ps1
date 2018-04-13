171dd9f1b189878ab40fb970c0389b16cd0d42cb
ledona <delano@ledona.net>
2018-04-13 17:23:45 -0400

    power shell scripts for dnn

--- 2018.04.13/nba_dnn_fd_win.ps1
+++ 2018.04.13/nba_dnn_fd_win.ps1
@@ -1,14 +1,14 @@
 # RUN ON WINDOWS
 
-OUTSTAT=dk_score#
+$Env:OUTSTAT = "fd_score#"
 
-meval_O.sc  --progress --cache_dir "./casedata_cache" `
---scoring mae r2 --seasons 2016 2015 2014 2013 2012 `
+python -O ".\scripts\meval_O.sc" --progress --cache_dir "./casedata_cache" `
+--scoring mae --seasons 2016 2015 2014 2013 2012 `
 --search_method bayes --search_iters 50 --search_bayes_init_pts 5 `
 --search_bayes_scorer mae `
 --folds 3 -o nba_fantasy_dnn_rs `
 nba.db keras `
---model_player_stat {OUTSTAT} `
+--model_player_stat $Env:OUTSTAT `
 --n_games_range 1 6 `
 --n_cases_range 500 10000 --n_cases_range_inc 500 `
 --in_player_stats asst blks home "*_reb" "fg*" fouls "ft_*" mins pts starter `
@@ -18,19 +18,19 @@ nba.db keras `
 --normalize `
 --steps_range 100 1000 --steps_range_inc 100 `
 --layers_range 1 5 `
---units 20 100 `
+--units_range 20 100 `
 --activation_list linear relu tanh sigmoid `
 --dropout_range .3 .7 `
 --learning_method_list rmsprop sgd `
 --lr_range .0005 .01
 
-meval_O.sc  --progress --cache_dir "./casedata_cache" `
---scoring mae r2 --seasons 2016 2015 2014 2013 2012 `
+python -O .\scripts\meval_O.sc --progress --cache_dir "./casedata_cache" `
+--scoring mae --seasons 2016 2015 2014 2013 2012 `
 --search_method bayes --search_iters 50 --search_bayes_init_pts 5 `
 --search_bayes_scorer mae `
 --folds 3 -o nba_fantasy_dnn_ada `
 nba.db keras `
---model_player_stat {OUTSTAT} `
+--model_player_stat $Env:OUTSTAT `
 --n_games_range 1 6 `
 --n_cases_range 500 10000 --n_cases_range_inc 500 `
 --in_player_stats asst blks home "*_reb" "fg*" fouls "ft_*" mins pts starter `
@@ -40,7 +40,7 @@ nba.db keras `
 --normalize `
 --steps_range 100 1000 --steps_range_inc 100 `
 --layers_range 1 5 `
---units 20 100 `
+--units_range 20 100 `
 --activation_list linear relu tanh sigmoid `
 --dropout_range .3 .7 `
 --learning_method_list adagrad adadelta adam adamax nadam
