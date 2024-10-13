b6b2b93e611d87d44e8f37a1e20b95140a32b40f
delano <delano@ledona.net>
2024-10-13 22:09:57 +0000

    filenaming

--- df-hist/lib/data_cfg.py
+++ df-hist/lib/data_cfg.py
@@ -9,19 +9,19 @@ SPORT_CFGS = {
     "mlb": {
         "min_date": date(2019, 1, 1),
         "max_date": date(2024, 1, 1),
-        "db_filename": os.path.join(_FANTASY_HOME, "mlb_hist_20082023.scored.db"),
+        "db_filename": os.path.join(_FANTASY_HOME, "mlb.hist.2008-2023.scored.db"),
         "cost_pos_drop": {"DH", "RP"},
         "cost_pos_rename": {"SP": "P"},
     },
     "nfl": {
         "min_date": date(2020, 1, 12),  # no NFL dfs slates before this date
         "max_date": date(2023, 4, 1),
-        "db_filename": os.path.join(_FANTASY_HOME, "nfl_hist_2009-2022.scored.db"),
+        "db_filename": os.path.join(_FANTASY_HOME, "nfl.hist.2009-2022.scored.db"),
     },
     "nba": {
         "min_date": {None: date(2017, 8, 1), "yahoo": date(2020, 8, 1)},
         "max_date": date(2023, 8, 1),
-        "db_filename": os.path.join(_FANTASY_HOME, "nba_hist_20082009-20222023.scored.db"),
+        "db_filename": os.path.join(_FANTASY_HOME, "nba.hist.20082009-20222023.scored.db"),
     },
     "nhl": {
         "min_date": {
@@ -30,11 +30,11 @@ SPORT_CFGS = {
             None: date(2017, 8, 1),
         },
         "max_date": date(2023, 4, 1),
-        "db_filename": os.path.join(_FANTASY_HOME, "nhl_hist_20072008-20222023.scored.db"),
+        "db_filename": os.path.join(_FANTASY_HOME, "nhl.hist.20072008-20222023.scored.db"),
         "cost_pos_rename": {"LW": "W", "RW": "W"},
     },
     "lol": {
-        "db_filename": os.path.join(_FANTASY_HOME, "lol_hist_2014-2022.scored.db"),
+        "db_filename": os.path.join(_FANTASY_HOME, "lol.hist.2014-2022.scored.db"),
         "min_date": date(2020, 1, 1),
         "max_date": date(2023, 1, 1),
         "services": ["draftkings", "fanduel"],
